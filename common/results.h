a7cca3f3628e675e2156f82e1ed2c1fb23a47f02
Hagen M?bius <hagen.moebius@googlemail.com>
2015-11-14 11:23:10 +0100

    Added a new inspector for wave files. This is very rudimenatry at the moment and is merely used to test out a new results/values framework which can be found in the directory common.

---
+++ common/results.h
@@ -0,0 +1,220 @@
+#ifndef COMMON_RESULTS_H
+#define COMMON_RESULTS_H
+
+#include <cstdint>
+#include <experimental/any>
+#include <list>
+#include <memory>
+#include <string>
+
+namespace Results
+{
+	class ValueBase
+	{
+	public:
+		ValueBase(void)
+		{
+		}
+		
+		ValueBase(const std::string & Name) :
+			_Name(Name)
+		{
+		}
+		
+		virtual ~ValueBase(void)
+		{
+		}
+		
+		const std::string & GetName(void) const
+		{
+			return _Name;
+		}
+		
+		void SetName(const std::string & Name)
+		{
+			_Name = Name;
+		}
+		
+		virtual std::experimental::any & GetValue(void) = 0;
+		
+		virtual std::list< std::shared_ptr< ValueBase > > & GetValues(void) = 0;
+	private:
+		std::string _Name;
+	};
+
+	class Value : public ValueBase
+	{
+	public:
+		Value(void)
+		{
+		}
+		
+		Value(const std::string & Name) :
+			ValueBase(Name)
+		{
+		}
+		
+		Value(const std::experimental::any & Value) :
+			_Value(Value)
+		{
+		}
+		
+		Value(const std::string & Name, const std::experimental::any & Value) :
+			ValueBase(Name),
+			_Value(Value)
+		{
+		}
+		
+		virtual ~Value(void)
+		{
+		}
+		
+		void SetValue(std::experimental::any & Value)
+		{
+			_Value = Value;
+		}
+		
+		virtual std::experimental::any & GetValue(void) override
+		{
+			return _Value;
+		}
+		
+		virtual std::list< std::shared_ptr< ValueBase > > & GetValues(void)
+		{
+			throw new std::exception();
+		}
+	private:
+		std::experimental::any _Value;
+	};
+
+	class Values : public ValueBase
+	{
+	public:
+		Values(void)
+		{
+		}
+		
+		Values(const std::string & Name) :
+			ValueBase(Name)
+		{
+		}
+		
+		virtual ~Values(void)
+		{
+		}
+		
+		void Append(std::shared_ptr< ValueBase > Value)
+		{
+			_Values.push_back(Value);
+		}
+		
+		void Append(const std::string & Name, std::shared_ptr< ValueBase > Value)
+		{
+			Value->SetName(Name);
+			_Values.push_back(Value);
+		}
+		
+		void Append(const std::string & Name, std::experimental::any & Value)
+		{
+			_Values.push_back(std::make_shared< Results::Value >(Name, Value));
+		}
+		
+		virtual std::experimental::any & GetValue(void) override
+		{
+			throw new std::exception();
+		}
+		
+		virtual std::list< std::shared_ptr< ValueBase > > & GetValues(void)
+		{
+			return _Values;
+		}
+	private:
+		std::list< std::shared_ptr< ValueBase > > _Values;
+	};
+
+	class Result
+	{
+	public:
+		Result(void)
+		{
+		}
+		
+		Result(bool Success, std::uint64_t Length, std::shared_ptr< ValueBase > Value) :
+			_Length(Length),
+			_Success(Success),
+			_Value(Value)
+		{
+		}
+		
+		~Result(void)
+		{
+		}
+		
+		std::uint64_t GetLength(void) const
+		{
+			return _Length;
+		}
+		
+		bool GetSuccess(void) const
+		{
+			return _Success;
+		}
+		
+		std::shared_ptr< ValueBase > & GetValue(void)
+		{
+			return _Value;
+		}
+		
+		std::shared_ptr< ValueBase > & GetValue(const std::string & Name)
+		{
+			for(auto & Value : _Value->GetValues())
+			{
+				if(Value->GetName() == Name)
+				{
+					return Value;
+				}
+			}
+			
+			throw new std::exception();
+		}
+		
+		std::experimental::any & Get(void)
+		{
+			return _Value->GetValue();
+		}
+		
+		std::experimental::any & Get(const std::string & Name)
+		{
+			for(auto & Value : _Value->GetValues())
+			{
+				if(Value->GetName() == Name)
+				{
+					return Value->GetValue();
+				}
+			}
+			
+			throw new std::exception();
+		}
+		
+		void SetLength(std::uint64_t Length)
+		{
+			_Length = Length;
+		}
+		
+		void SetSuccess(bool Success)
+		{
+			_Success = Success;
+		}
+		
+		void SetValue(std::shared_ptr< ValueBase > Value)
+		{
+			_Value = Value;
+		}
+	private:
+		std::uint64_t _Length;
+		bool _Success;
+		std::shared_ptr< ValueBase > _Value;
+	};
+}
+
+#endif
