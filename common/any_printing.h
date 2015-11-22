0f2892486be6814c3696b5d258ed70ca5f3841f3
Hagen Möbius <hagen.moebius@googlemail.com>
2015-11-22 16:39:08 +0100

    Added a specialized function which returns a 32bit value stored in little endian format as a bitset<32> value. The ChannelMask is now processed with that new reader function. Added a writing specialization for bitset<32> values in an any instance.

--- common/any_printing.h
+++ common/any_printing.h
@@ -1,6 +1,7 @@
 #ifndef COMON_ANY_PRINTING_H
 #define COMON_ANY_PRINTING_H
 
+#include <bitset>
 #include <cstdint>
 #include <experimental/any>
 #include <ostream>
@@ -19,6 +20,10 @@ inline std::ostream & operator<<(std::ostream & OStream, const std::experimental
 	else if(Any.type() == typeid(std::uint16_t))
 	{
 		return OStream << std::experimental::any_cast< std::uint16_t >(Any);
+	}
+	else if(Any.type() == typeid(std::bitset<32>))
+	{
+		return OStream << std::experimental::any_cast< std::bitset<32> >(Any);
 	}
 	else
 	{
