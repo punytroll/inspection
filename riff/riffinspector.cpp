317816f4e96ef89259a29557855aacf9f65444b9
Hagen Möbius <hagen.moebius@googlemail.com>
2015-11-22 20:24:00 +0100

    A specialized getter function for the channel mask now tries a rudimentary interpretation of the bit field.

--- riff/riffinspector.cpp
+++ riff/riffinspector.cpp
@@ -21,6 +21,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 std::unique_ptr< Results::Result > Get_ASCII_AlphaNumericStringWithSpaceTerminatedByLength(const std::uint8_t * Buffer, std::uint64_t Length);
 std::unique_ptr< Results::Result > Get_ASCII_AlphaNumericOrSpaceCharacter(const std::uint8_t * Buffer, std::uint64_t Length);
+std::unique_ptr< Results::Result > Get_ChannelMask(const std::uint8_t * Buffer, std::uint64_t Length);
 std::unique_ptr< Results::Result > Get_FormatTag(const std::uint8_t * Buffer, std::uint64_t Length);
 std::unique_ptr< Results::Result > Get_HexadecimalString_TerminatedByLength(const std::uint8_t * Buffer, std::uint64_t Length);
 std::unique_ptr< Results::Result > Get_LittleEndian_16Bit_UnsignedInteger(const std::uint8_t * Buffer, std::uint64_t Length);
@@ -75,6 +76,114 @@ std::unique_ptr< Results::Result > Get_ASCII_AlphaNumericOrSpaceCharacter(const 
 	}
 	
 	return std::unique_ptr< Results::Result >(new Results::Result(Success, Index, std::make_shared< Results::Value >(Value)));
+}
+
+std::unique_ptr< Results::Result > Get_ChannelMask(const std::uint8_t * Buffer, std::uint64_t Length)
+{
+	auto Success{false};
+	auto Index{0ull};
+	auto Value{std::make_shared< Results::Value >()};
+	auto ChannelMaskResult{Get_LittleEndian_32Bit_BitSet(Buffer + Index, Length - Index)};
+	
+	if(ChannelMaskResult->GetSuccess() == true)
+	{
+		Index += ChannelMaskResult->GetLength();
+		Value->SetAny(ChannelMaskResult->GetAny());
+		Success = true;
+		
+		auto & ChannelMask{std::experimental::any_cast< const std::bitset< 32 > & >(ChannelMaskResult->GetAny())};
+		
+		if(ChannelMask[0] == true)
+		{
+			Value->Append(std::string("SPEAKER_FRONT_LEFT"));
+		}
+		if(ChannelMask[1] == true)
+		{
+			Value->Append(std::string("SPEAKER_FRONT_RIGHT"));
+		}
+		if(ChannelMask[2] == true)
+		{
+			Value->Append(std::string("SPEAKER_FRONT_CENTER"));
+		}
+		if(ChannelMask[3] == true)
+		{
+			Value->Append(std::string("SPEAKER_LOW_FREQUENCY"));
+		}
+		if(ChannelMask[4] == true)
+		{
+			Value->Append(std::string("SPEAKER_BACK_LEFT"));
+		}
+		if(ChannelMask[5] == true)
+		{
+			Value->Append(std::string("SPEAKER_BACK_RIGHT"));
+		}
+		if(ChannelMask[6] == true)
+		{
+			Value->Append(std::string("SPEAKER_FRONT_LEFT_OF_CENTER"));
+		}
+		if(ChannelMask[7] == true)
+		{
+			Value->Append(std::string("SPEAKER_FRONT_RIGHT_OF_CENTER"));
+		}
+		if(ChannelMask[8] == true)
+		{
+			Value->Append(std::string("SPEAKER_BACK_CENTER"));
+		}
+		if(ChannelMask[9] == true)
+		{
+			Value->Append(std::string("SPEAKER_SIDE_LEFT"));
+		}
+		if(ChannelMask[10] == true)
+		{
+			Value->Append(std::string("SPEAKER_SIDE_RIGHT"));
+		}
+		if(ChannelMask[11] == true)
+		{
+			Value->Append(std::string("SPEAKER_TOP_CENTER"));
+		}
+		if(ChannelMask[12] == true)
+		{
+			Value->Append(std::string("SPEAKER_TOP_FRONT_LEFT"));
+		}
+		if(ChannelMask[13] == true)
+		{
+			Value->Append(std::string("SPEAKER_TOP_FRONT_CENTER"));
+		}
+		if(ChannelMask[14] == true)
+		{
+			Value->Append(std::string("SPEAKER_TOP_FRONT_RIGHT"));
+		}
+		if(ChannelMask[15] == true)
+		{
+			Value->Append(std::string("SPEAKER_TOP_BACK_LEFT"));
+		}
+		if(ChannelMask[16] == true)
+		{
+			Value->Append(std::string("SPEAKER_TOP_BACK_CENTER"));
+		}
+		if(ChannelMask[17] == true)
+		{
+			Value->Append(std::string("SPEAKER_TOP_BACK_RIGHT"));
+		}
+		/// @todo bits 18 to 30 are reserved and should be warned against
+		if(ChannelMask[31] == true)
+		{
+			Value->Append(std::string("SPEAKER_ALL"));
+		}
+		/// @todo there are the following bit combinations which have special names
+		//~ #define SPEAKER_MONO             (SPEAKER_FRONT_CENTER)
+		//~ #define SPEAKER_STEREO           (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT)
+		//~ #define SPEAKER_2POINT1          (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_LOW_FREQUENCY)
+		//~ #define SPEAKER_SURROUND         (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_BACK_CENTER)
+		//~ #define SPEAKER_QUAD             (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT)
+		//~ #define SPEAKER_4POINT1          (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_LOW_FREQUENCY | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT)
+		//~ #define SPEAKER_5POINT1          (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_LOW_FREQUENCY | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT)
+		//~ #define SPEAKER_7POINT1          (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_LOW_FREQUENCY | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT | SPEAKER_FRONT_LEFT_OF_CENTER | SPEAKER_FRONT_RIGHT_OF_CENTER)
+		//~ #define SPEAKER_5POINT1_SURROUND (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_LOW_FREQUENCY | SPEAKER_SIDE_LEFT | SPEAKER_SIDE_RIGHT)
+		//~ #define SPEAKER_7POINT1_SURROUND (SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT | SPEAKER_FRONT_CENTER | SPEAKER_LOW_FREQUENCY | SPEAKER_BACK_LEFT | SPEAKER_BACK_RIGHT | SPEAKER_SIDE_LEFT  | SPEAKER_SIDE_RIGHT)
+	}
+	
+	return std::unique_ptr< Results::Result >(new Results::Result(Success, Index, Value));
 }
 
 std::unique_ptr< Results::Result > Get_FormatTag(const std::uint8_t * Buffer, std::uint64_t Length)
@@ -445,7 +554,7 @@ std::unique_ptr< Results::Result > Get_RIFF_fmt_ChunkData(const std::uint8_t * B
 												Index += ValidBitsPerSampleResult->GetLength();
 												Value->Append("ValidBitsPerSample", ValidBitsPerSampleResult->GetValue());
 												
-												auto ChannelMaskResult{Get_LittleEndian_32Bit_BitSet(Buffer + Index, Length - Index)};
+												auto ChannelMaskResult{Get_ChannelMask(Buffer + Index, Length - Index)};
 												
 												if(ChannelMaskResult->GetSuccess() == true)
 												{
@@ -528,17 +637,19 @@ std::unique_ptr< Results::Result > Get_RIFF_RIFF_ChunkData(const std::uint8_t * 
 
 void PrintValue(const std::string & Indentation, std::shared_ptr< Results::Value > Value)
 {
-	auto HeaderLine{false};
+	auto HeaderLine{(Value->GetName().empty() == false) || (Value->GetAny().empty() == false)};
 	
+	if(HeaderLine == true)
+	{
+		std::cout << Indentation;
+	}
 	if(Value->GetName().empty() == false)
 	{
-		std::cout << Indentation << Value->GetName() << ": ";
-		HeaderLine = true;
+		std::cout << Value->GetName() << ": ";
 	}
 	if(Value->GetAny().empty() == false)
 	{
 		std::cout << Value->GetAny();
-		HeaderLine = true;
 	}
 	if(HeaderLine == true)
 	{
