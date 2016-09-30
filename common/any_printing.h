cd6ec98a4ff158a7d0a8a041d580028ce490b608
Hagen Möbius <hagen.moebius@googlemail.com>
2016-09-30 04:58:13 +0200

    Added more power to the any printing operator<<. More bitsets, more integer values.

--- common/any_printing.h
+++ common/any_printing.h
@@ -13,21 +13,33 @@ inline std::ostream & operator<<(std::ostream & OStream, const std::experimental
 	{
 		return OStream << std::experimental::any_cast< std::string >(Any);
 	}
+	else if(Any.type() == typeid(std::uint16_t))
+	{
+		return OStream << std::experimental::any_cast< std::uint16_t >(Any);
+	}
 	else if(Any.type() == typeid(std::uint32_t))
 	{
 		return OStream << std::experimental::any_cast< std::uint32_t >(Any);
 	}
-	else if(Any.type() == typeid(std::uint16_t))
+	else if(Any.type() == typeid(std::uint64_t))
 	{
-		return OStream << std::experimental::any_cast< std::uint16_t >(Any);
+		return OStream << std::experimental::any_cast< std::uint64_t >(Any);
+	}
+	else if(Any.type() == typeid(std::uint8_t))
+	{
+		return OStream << static_cast< std::uint32_t >(std::experimental::any_cast< std::uint8_t >(Any));
 	}
 	else if(Any.type() == typeid(std::bitset<32>))
 	{
 		return OStream << std::experimental::any_cast< std::bitset<32> >(Any);
 	}
+	else if(Any.type() == typeid(std::bitset<8>))
+	{
+		return OStream << std::experimental::any_cast< std::bitset<8> >(Any);
+	}
 	else
 	{
-		return OStream << '<' << Any.type().name() << '>';
+		return OStream << "<Unknown any type: " << Any.type().name() << '>';
 	}
 }
 
