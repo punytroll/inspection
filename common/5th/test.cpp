61e9a3cc6606fbfab265c4b5a6b9cdc3adcbf59a
Hagen Möbius <hagen.moebius@googlemail.com>
2017-03-15 13:13:53 +0100

    Added a function to read 7 bits. Added a function to read 24 bits into an array of bytes. This is the way that all reading functions should return their value. Extended the testing suite to test for seven and 24 bits. Some minor cosmetics.

--- common/5th/test.cpp
+++ common/5th/test.cpp
@@ -4,7 +4,7 @@
 
 #include "buffer.h"
 
-void TestStartingPositionInDEADBEEF(void)
+void TestStartingPosition(void)
 {
 	std::uint8_t Data[4];
 	
@@ -19,7 +19,7 @@ void TestStartingPositionInDEADBEEF(void)
 	assert(Buffer.GetPosition().GetBits() == 0);
 }
 
-void TestBytesInDEADBEEF(void)
+void Test8Bits(void)
 {
 	std::uint8_t Data[4];
 	
@@ -31,19 +31,19 @@ void TestBytesInDEADBEEF(void)
 	Inspection::Buffer Buffer{Data, 4};
 	
 	// four bytes
-	assert(Buffer.Get1Byte() == 0xde);
+	assert(Buffer.Get8Bits() == 0xde);
 	assert(Buffer.GetPosition().GetBytes() == 1);
 	assert(Buffer.GetPosition().GetBits() == 0);
-	assert(Buffer.Get1Byte() == 0xad);
+	assert(Buffer.Get8Bits() == 0xad);
 	assert(Buffer.GetPosition().GetBytes() == 2);
 	assert(Buffer.GetPosition().GetBits() == 0);
-	assert(Buffer.Get1Byte() == 0xbe);
+	assert(Buffer.Get8Bits() == 0xbe);
 	assert(Buffer.GetPosition().GetBytes() == 3);
 	assert(Buffer.GetPosition().GetBits() == 0);
-	assert(Buffer.Get1Byte() == 0xef);
+	assert(Buffer.Get8Bits() == 0xef);
 }
 
-void TestBytesOffsetOneInDEADBEEF(void)
+void Test8BitsOffsetOne(void)
 {
 	std::uint8_t Data[4];
 	
@@ -59,13 +59,13 @@ void TestBytesOffsetOneInDEADBEEF(void)
 	assert(Buffer.GetPosition().GetBytes() == 0);
 	assert(Buffer.GetPosition().GetBits() == 1);
 	// three bytes
-	assert(Buffer.Get1Byte() == 0xbd);
+	assert(Buffer.Get8Bits() == 0xbd);
 	assert(Buffer.GetPosition().GetBytes() == 1);
 	assert(Buffer.GetPosition().GetBits() == 1);
-	assert(Buffer.Get1Byte() == 0x5b);
+	assert(Buffer.Get8Bits() == 0x5b);
 	assert(Buffer.GetPosition().GetBytes() == 2);
 	assert(Buffer.GetPosition().GetBits() == 1);
-	assert(Buffer.Get1Byte() == 0x7d);
+	assert(Buffer.Get8Bits() == 0x7d);
 	assert(Buffer.GetPosition().GetBytes() == 3);
 	assert(Buffer.GetPosition().GetBits() == 1);
 	// remaining seven bits
@@ -92,7 +92,7 @@ void TestBytesOffsetOneInDEADBEEF(void)
 	assert(Buffer.GetPosition().GetBits() == 0);
 }
 
-void TestBitsInDEADBEEF(void)
+void Test1Bit(void)
 {
 	std::uint8_t Data[4];
 	
@@ -209,12 +209,109 @@ void TestBitsInDEADBEEF(void)
 	assert(Buffer.GetPosition().GetBits() == 0);
 }
 
+void Test7Bits(void)
+{
+	std::uint8_t Data[8];
+	
+	Data[0] = 0xde;
+	Data[1] = 0xad;
+	Data[2] = 0xbe;
+	Data[3] = 0xef;
+	Data[4] = 0xde;
+	Data[5] = 0xad;
+	Data[6] = 0xbe;
+	Data[7] = 0xef;
+	
+	Inspection::Buffer Buffer{Data, 8};
+	
+	assert(Buffer.Get7Bits() == 0x6f);
+	assert(Buffer.GetPosition().GetBytes() == 0);
+	assert(Buffer.GetPosition().GetBits() == 7);
+	assert(Buffer.Get7Bits() == 0x2b);
+	assert(Buffer.GetPosition().GetBytes() == 1);
+	assert(Buffer.GetPosition().GetBits() == 6);
+	assert(Buffer.Get7Bits() == 0x37);
+	assert(Buffer.GetPosition().GetBytes() == 2);
+	assert(Buffer.GetPosition().GetBits() == 5);
+	assert(Buffer.Get7Bits() == 0x6e);
+	assert(Buffer.GetPosition().GetBytes() == 3);
+	assert(Buffer.GetPosition().GetBits() == 4);
+	assert(Buffer.Get7Bits() == 0x7e);
+	assert(Buffer.GetPosition().GetBytes() == 4);
+	assert(Buffer.GetPosition().GetBits() == 3);
+	assert(Buffer.Get7Bits() == 0x7a);
+	assert(Buffer.GetPosition().GetBytes() == 5);
+	assert(Buffer.GetPosition().GetBits() == 2);
+	assert(Buffer.Get7Bits() == 0x5b);
+	assert(Buffer.GetPosition().GetBytes() == 6);
+	assert(Buffer.GetPosition().GetBits() == 1);
+	assert(Buffer.Get7Bits() == 0x3e);
+	assert(Buffer.GetPosition().GetBytes() == 7);
+	assert(Buffer.GetPosition().GetBits() == 0);
+}
+
+void Test24Bits(void)
+{
+	std::uint8_t Data[12];
+	
+	Data[0] = 0xde;
+	Data[1] = 0xad;
+	Data[2] = 0xbe;
+	Data[3] = 0xef;
+	Data[4] = 0xde;
+	Data[5] = 0xad;
+	Data[6] = 0xbe;
+	Data[7] = 0xef;
+	Data[8] = 0xde;
+	Data[9] = 0xad;
+	Data[10] = 0xbe;
+	Data[11] = 0xef;
+	
+	Inspection::Buffer Buffer{Data, 12};
+	
+	assert((Buffer.Get24Bits() == std::array< std::uint8_t, 3 >({0xde, 0xad, 0xbe})));
+	assert((Buffer.Get24Bits() == std::array< std::uint8_t, 3 >({0xef, 0xde, 0xad})));
+	assert((Buffer.Get24Bits() == std::array< std::uint8_t, 3 >({0xbe, 0xef, 0xde})));
+	assert((Buffer.Get24Bits() == std::array< std::uint8_t, 3 >({0xad, 0xbe, 0xef})));
+}
+
+void Test24BitsWithOffsets(void)
+{
+	std::uint8_t Data[12];
+	
+	Data[0] = 0xde;
+	Data[1] = 0xad;
+	Data[2] = 0xbe;
+	Data[3] = 0xef;
+	Data[4] = 0xde;
+	Data[5] = 0xad;
+	Data[6] = 0xbe;
+	Data[7] = 0xef;
+	Data[8] = 0xde;
+	Data[9] = 0xad;
+	Data[10] = 0xbe;
+	Data[11] = 0xef;
+	
+	Inspection::Buffer Buffer{Data, 12};
+	
+	assert(Buffer.Get1Bit() == 0x01);
+	assert((Buffer.Get24Bits() == std::array< std::uint8_t, 3 >({0xbd, 0x5b, 0x7d})));
+	assert(Buffer.Get1Bit() == 0x01);
+	assert((Buffer.Get24Bits() == std::array< std::uint8_t, 3 >({0xbf, 0x7a, 0xb6})));
+	assert(Buffer.Get1Bit() == 0x01);
+	assert((Buffer.Get24Bits() == std::array< std::uint8_t, 3 >({0xf7, 0x7e, 0xf5})));
+	assert(Buffer.Get1Bit() == 0x00);
+}
+
 int main(int argc, char ** argv)
 {
-	TestStartingPositionInDEADBEEF();
-	TestBytesInDEADBEEF();
-	TestBitsInDEADBEEF();
-	TestBytesOffsetOneInDEADBEEF();
+	TestStartingPosition();
+	Test8Bits();
+	Test1Bit();
+	Test8BitsOffsetOne();
+	Test7Bits();
+	Test24Bits();
+	Test24BitsWithOffsets();
 	
 	return 0;
 }
