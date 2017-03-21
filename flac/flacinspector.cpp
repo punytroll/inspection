b6f66efad40cb52785992e0d5fbb6329586b7fc0
Hagen Möbius <hagen.moebius@googlemail.com>
2017-03-21 16:02:04 +0100

    Added reading the picture data block, without interpreting the image content. But at least we get the actual meta data and can skip over the picture data.

--- flac/flacinspector.cpp
+++ flac/flacinspector.cpp
@@ -18,6 +18,8 @@ std::unique_ptr< Inspection::Result > Get_FLAC_MetaDataBlock(Inspection::Buffer 
 std::unique_ptr< Inspection::Result > Get_FLAC_MetaDataBlockHeader(Inspection::Buffer & Buffer);
 std::unique_ptr< Inspection::Result > Get_FLAC_MetaDataBlockType(Inspection::Buffer & Buffer);
 std::unique_ptr< Inspection::Result > Get_FLAC_NumberOfChannels(Inspection::Buffer & Buffer);
+std::unique_ptr< Inspection::Result > Get_FLAC_PictureBlock_PictureType(Inspection::Buffer & Buffer);
+std::unique_ptr< Inspection::Result > Get_FLAC_PictureBlockData(Inspection::Buffer & Buffer);
 std::unique_ptr< Inspection::Result > Get_FLAC_SeekPoint(Inspection::Buffer & Buffer);
 std::unique_ptr< Inspection::Result > Get_FLAC_SeekTableBlockData(Inspection::Buffer & Buffer, std::uint32_t NumberOfSeekPoints);
 std::unique_ptr< Inspection::Result > Get_FLAC_Stream(Inspection::Buffer & Buffer);
@@ -96,6 +98,16 @@ std::unique_ptr< Inspection::Result > Get_FLAC_MetaDataBlock(Inspection::Buffer 
 			if(VorbisCommentBlockDataResult->GetSuccess() == true)
 			{
 				Result->GetValue()->Append("Data", VorbisCommentBlockDataResult->GetValue());
+				Result->SetSuccess(true);
+			}
+		}
+		else if(MetaDataBlockType == "Picture")
+		{
+			auto PictureBlockDataResult{Get_FLAC_PictureBlockData(Buffer)};
+			
+			if(PictureBlockDataResult->GetSuccess() == true)
+			{
+				Result->GetValue()->Append("Data", PictureBlockDataResult->GetValue());
 				Result->SetSuccess(true);
 			}
 		}
@@ -199,6 +211,200 @@ std::unique_ptr< Inspection::Result > Get_FLAC_NumberOfChannels(Inspection::Buff
 		Result->SetValue(NumberOfChannelsResult->GetValue());
 		Result->GetValue()->Append(Inspection::MakeValue("Interpretation", static_cast< std::uint8_t >(std::experimental::any_cast< std::uint8_t >(NumberOfChannelsResult->GetAny()) + 1)));
 		Result->SetSuccess(true);
+	}
+	Inspection::FinalizeResult(Result, Buffer);
+	
+	return Result;
+}
+
+std::unique_ptr< Inspection::Result > Get_FLAC_PictureBlock_PictureType(Inspection::Buffer & Buffer)
+{
+	auto Result{Inspection::InitializeResult(false, Buffer)};
+	auto PictureTypeResult{Get_UnsignedInteger_32Bit_BigEndian(Buffer)};
+	
+	if(PictureTypeResult->GetSuccess() == true)
+	{
+		Result->SetValue(PictureTypeResult->GetValue());
+		Result->SetSuccess(true);
+		
+		auto PictureType{std::experimental::any_cast< std::uint32_t >(PictureTypeResult->GetAny())};
+		
+		if(PictureType == 0)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Other")));
+		}
+		else if(PictureType == 1)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("32x32 pixels 'file icon' (PNG only)")));
+		}
+		else if(PictureType == 2)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Other file icon")));
+		}
+		else if(PictureType == 3)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Cover (front)")));
+		}
+		else if(PictureType == 4)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Cover (back)")));
+		}
+		else if(PictureType == 5)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Leaflet page")));
+		}
+		else if(PictureType == 6)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Media (e.g. label side of CD")));
+		}
+		else if(PictureType == 7)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Lead artist/lead performer/soloist")));
+		}
+		else if(PictureType == 8)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Artist/performer")));
+		}
+		else if(PictureType == 9)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Conductor")));
+		}
+		else if(PictureType == 10)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Band/Orchestra")));
+		}
+		else if(PictureType == 11)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Composer")));
+		}
+		else if(PictureType == 12)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Lyricist/text writer")));
+		}
+		else if(PictureType == 13)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Recording Location")));
+		}
+		else if(PictureType == 14)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("During recording")));
+		}
+		else if(PictureType == 15)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("During performance")));
+		}
+		else if(PictureType == 16)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Movie/video screen capture")));
+		}
+		else if(PictureType == 17)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("A bright coloured fish")));
+		}
+		else if(PictureType == 18)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Illustration")));
+		}
+		else if(PictureType == 19)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Band/artist logotype")));
+		}
+		else if(PictureType == 20)
+		{
+			Result->GetValue()->Append(Inspection::MakeValue("Interpretation", std::string("Publisher/Studio logotype")));
+		}
+		else
+		{
+			Result->SetSuccess(false);
+		}
+	}
+	Inspection::FinalizeResult(Result, Buffer);
+	
+	return Result;
+}
+
+std::unique_ptr< Inspection::Result > Get_FLAC_PictureBlockData(Inspection::Buffer & Buffer)
+{
+	auto Result{Inspection::InitializeResult(false, Buffer)};
+	auto PictureTypeResult{Get_FLAC_PictureBlock_PictureType(Buffer)};
+	
+	if(PictureTypeResult->GetSuccess() == true)
+	{
+		Result->GetValue()->Append("PictureType", PictureTypeResult->GetValue());
+		
+		auto MIMETypeLengthResult{Get_UnsignedInteger_32Bit_BigEndian(Buffer)};
+		
+		if(MIMETypeLengthResult->GetSuccess() == true)
+		{
+			Result->GetValue()->Append("MIMETypeLength", MIMETypeLengthResult->GetValue());
+			
+			auto MIMETypeLength{std::experimental::any_cast< std::uint32_t >(MIMETypeLengthResult->GetAny())};
+			auto MIMETypeResult{Get_ASCII_String_Printable_EndedByByteLength(Buffer, MIMETypeLength)};
+			
+			if(MIMETypeResult->GetSuccess() == true)
+			{
+				Result->GetValue()->Append("MIMType", MIMETypeResult->GetValue());
+				
+				auto DescriptionLengthResult{Get_UnsignedInteger_32Bit_BigEndian(Buffer)};
+				
+				if(DescriptionLengthResult->GetSuccess() == true)
+				{
+					Result->GetValue()->Append("DescriptionLength", DescriptionLengthResult->GetValue());
+					
+					auto DescriptionLength{std::experimental::any_cast< std::uint32_t >(DescriptionLengthResult->GetAny())};
+					auto DescriptionResult{Get_UTF8_String_EndedByByteLength(Buffer, DescriptionLength)};
+					
+					if(DescriptionResult->GetSuccess() == true)
+					{
+						Result->GetValue()->Append("Description", DescriptionResult->GetValue());
+						
+						auto PictureWidthInPixelsResult{Get_UnsignedInteger_32Bit_BigEndian(Buffer)};
+						
+						if(PictureWidthInPixelsResult->GetSuccess() == true)
+						{
+							Result->GetValue()->Append("PictureWidthInPixels", PictureWidthInPixelsResult->GetValue());
+							
+							auto PictureHeightInPixelsResult{Get_UnsignedInteger_32Bit_BigEndian(Buffer)};
+							
+							if(PictureHeightInPixelsResult->GetSuccess() == true)
+							{
+								Result->GetValue()->Append("PictureHeightInPixels", PictureHeightInPixelsResult->GetValue());
+								
+								auto BitsPerPixelResult{Get_UnsignedInteger_32Bit_BigEndian(Buffer)};
+								
+								if(BitsPerPixelResult->GetSuccess() == true)
+								{
+									Result->GetValue()->Append("BitsPerPixel", BitsPerPixelResult->GetValue());
+									
+									auto NumberOfColorsResult{Get_UnsignedInteger_32Bit_BigEndian(Buffer)};
+									
+									if(NumberOfColorsResult->GetSuccess() == true)
+									{
+										Result->GetValue()->Append("NumberOfColors", NumberOfColorsResult->GetValue());
+										
+										auto PictureDataLengthResult{Get_UnsignedInteger_32Bit_BigEndian(Buffer)};
+										
+										if(PictureDataLengthResult->GetSuccess() == true)
+										{
+											Result->GetValue()->Append("PictureDataLength", PictureDataLengthResult->GetValue());
+											
+											auto PictureDataLength{std::experimental::any_cast< std::uint32_t >(PictureDataLengthResult->GetAny())};
+											auto PictureDataResult{Get_Buffer_UnsignedInteger_8Bit_EndedByLength(Buffer, PictureDataLength)};
+											
+											if(PictureDataResult->GetSuccess() == true)
+											{
+												Result->GetValue()->Append("PictureData", PictureDataResult->GetValue());
+												Result->SetSuccess(true);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 	Inspection::FinalizeResult(Result, Buffer);
 	
