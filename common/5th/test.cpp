c61a8494bd7656c84ab01084f8d0e2b13ae312a1
Hagen M?bius <hagen.moebius@googlemail.com>
2017-03-22 09:00:21 +0100

    The testcases can now use a function SetupDataAndBuffer to initialize the testing ground with 16 bytes of data in a Buffer instance. Added tests for getters.

--- common/5th/test.cpp
+++ common/5th/test.cpp
@@ -1,251 +1,253 @@
 #include <cassert>
 
 #include "buffer.h"
+#include "getters.h"
+
+std::uint8_t g_Data[16];
+Inspection::Buffer * g_Buffer(nullptr);
+
+void SetupDataAndBuffer(void)
+{
+	g_Data[0] = 0xde;
+	g_Data[1] = 0xad;
+	g_Data[2] = 0xbe;
+	g_Data[3] = 0xef;
+	g_Data[4] = 0xde;
+	g_Data[5] = 0xad;
+	g_Data[6] = 0xbe;
+	g_Data[7] = 0xef;
+	g_Data[8] = 0xde;
+	g_Data[9] = 0xad;
+	g_Data[10] = 0xbe;
+	g_Data[11] = 0xef;
+	g_Data[12] = 0xde;
+	g_Data[13] = 0xad;
+	g_Data[14] = 0xbe;
+	g_Data[15] = 0xef;
+	delete g_Buffer;
+	g_Buffer = new Inspection::Buffer(g_Data, 16);
+}
 
 void TestStartingPosition(void)
 {
-	std::uint8_t Data[4];
-	
-	Data[0] = 0xde;
-	Data[1] = 0xad;
-	Data[2] = 0xbe;
-	Data[3] = 0xef;
-	
-	Inspection::Buffer Buffer{Data, 4};
-	
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 0);
+	SetupDataAndBuffer();
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
 }
 
 void Test8Bits(void)
 {
-	std::uint8_t Data[4];
-	
-	Data[0] = 0xde;
-	Data[1] = 0xad;
-	Data[2] = 0xbe;
-	Data[3] = 0xef;
-	
-	Inspection::Buffer Buffer{Data, 4};
-	
+	SetupDataAndBuffer();
 	// four bytes
-	assert(Buffer.Get8Bits() == 0xde);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 0);
-	assert(Buffer.Get8Bits() == 0xad);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 0);
-	assert(Buffer.Get8Bits() == 0xbe);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 0);
-	assert(Buffer.Get8Bits() == 0xef);
+	assert(g_Buffer->Get8Bits() == 0xde);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
+	assert(g_Buffer->Get8Bits() == 0xad);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
+	assert(g_Buffer->Get8Bits() == 0xbe);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
+	assert(g_Buffer->Get8Bits() == 0xef);
 }
 
 void Test8BitsOffsetOne(void)
 {
-	std::uint8_t Data[4];
-	
-	Data[0] = 0xde;
-	Data[1] = 0xad;
-	Data[2] = 0xbe;
-	Data[3] = 0xef;
-	
-	Inspection::Buffer Buffer{Data, 4};
-	
+	SetupDataAndBuffer();
 	// first bit
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 1);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 1);
 	// three bytes
-	assert(Buffer.Get8Bits() == 0xbd);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 1);
-	assert(Buffer.Get8Bits() == 0x5b);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 1);
-	assert(Buffer.Get8Bits() == 0x7d);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 1);
+	assert(g_Buffer->Get8Bits() == 0xbd);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 1);
+	assert(g_Buffer->Get8Bits() == 0x5b);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 1);
+	assert(g_Buffer->Get8Bits() == 0x7d);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 1);
 	// remaining seven bits
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 2);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 3);
-	assert(Buffer.Get1Bit() == 0x00);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 4);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 5);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 6);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 7);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 4);
-	assert(Buffer.GetPosition().GetBits() == 0);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 2);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 3);
+	assert(g_Buffer->Get1Bit() == 0x00);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 4);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 5);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 6);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 7);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 4);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
 }
 
 void Test1Bit(void)
 {
-	std::uint8_t Data[4];
-	
-	Data[0] = 0xde;
-	Data[1] = 0xad;
-	Data[2] = 0xbe;
-	Data[3] = 0xef;
-	
-	Inspection::Buffer Buffer{Data, 4};
-	
+	SetupDataAndBuffer();
 	// 'd'
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 1);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 2);
-	assert(Buffer.Get1Bit() == 0x00);
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 3);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 4);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 1);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 2);
+	assert(g_Buffer->Get1Bit() == 0x00);
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 3);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 4);
 	// 'e'
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 5);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 6);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 7);
-	assert(Buffer.Get1Bit() == 0x00);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 0);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 5);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 6);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 7);
+	assert(g_Buffer->Get1Bit() == 0x00);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
 	// 'a'
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 1);
-	assert(Buffer.Get1Bit() == 0x00);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 2);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 3);
-	assert(Buffer.Get1Bit() == 0x00);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 4);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 1);
+	assert(g_Buffer->Get1Bit() == 0x00);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 2);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 3);
+	assert(g_Buffer->Get1Bit() == 0x00);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 4);
 	// 'd'
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 5);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 6);
-	assert(Buffer.Get1Bit() == 0x00);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 7);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 0);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 5);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 6);
+	assert(g_Buffer->Get1Bit() == 0x00);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 7);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
 	// 'b'
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 1);
-	assert(Buffer.Get1Bit() == 0x00);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 2);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 3);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 4);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 1);
+	assert(g_Buffer->Get1Bit() == 0x00);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 2);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 3);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 4);
 	// 'e'
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 5);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 6);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 7);
-	assert(Buffer.Get1Bit() == 0x00);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 0);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 5);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 6);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 7);
+	assert(g_Buffer->Get1Bit() == 0x00);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
 	// 'e'
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 1);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 2);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 3);
-	assert(Buffer.Get1Bit() == 0x00);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 4);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 1);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 2);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 3);
+	assert(g_Buffer->Get1Bit() == 0x00);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 4);
 	// 'f'
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 5);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 6);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 7);
-	assert(Buffer.Get1Bit() == 0x01);
-	assert(Buffer.GetPosition().GetBytes() == 4);
-	assert(Buffer.GetPosition().GetBits() == 0);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 5);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 6);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 7);
+	assert(g_Buffer->Get1Bit() == 0x01);
+	assert(g_Buffer->GetPosition().GetBytes() == 4);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
 }
 
 void Test7Bits(void)
 {
-	std::uint8_t Data[8];
+	SetupDataAndBuffer();
+	assert(g_Buffer->Get7Bits() == 0x6f);
+	assert(g_Buffer->GetPosition().GetBytes() == 0);
+	assert(g_Buffer->GetPosition().GetBits() == 7);
+	assert(g_Buffer->Get7Bits() == 0x2b);
+	assert(g_Buffer->GetPosition().GetBytes() == 1);
+	assert(g_Buffer->GetPosition().GetBits() == 6);
+	assert(g_Buffer->Get7Bits() == 0x37);
+	assert(g_Buffer->GetPosition().GetBytes() == 2);
+	assert(g_Buffer->GetPosition().GetBits() == 5);
+	assert(g_Buffer->Get7Bits() == 0x6e);
+	assert(g_Buffer->GetPosition().GetBytes() == 3);
+	assert(g_Buffer->GetPosition().GetBits() == 4);
+	assert(g_Buffer->Get7Bits() == 0x7e);
+	assert(g_Buffer->GetPosition().GetBytes() == 4);
+	assert(g_Buffer->GetPosition().GetBits() == 3);
+	assert(g_Buffer->Get7Bits() == 0x7a);
+	assert(g_Buffer->GetPosition().GetBytes() == 5);
+	assert(g_Buffer->GetPosition().GetBits() == 2);
+	assert(g_Buffer->Get7Bits() == 0x5b);
+	assert(g_Buffer->GetPosition().GetBytes() == 6);
+	assert(g_Buffer->GetPosition().GetBits() == 1);
+	assert(g_Buffer->Get7Bits() == 0x3e);
+	assert(g_Buffer->GetPosition().GetBytes() == 7);
+	assert(g_Buffer->GetPosition().GetBits() == 0);
+}
+
+void Test_Get_SignedInteger_32Bit_BigEndian(void)
+{
+	SetupDataAndBuffer();
 	
-	Data[0] = 0xde;
-	Data[1] = 0xad;
-	Data[2] = 0xbe;
-	Data[3] = 0xef;
-	Data[4] = 0xde;
-	Data[5] = 0xad;
-	Data[6] = 0xbe;
-	Data[7] = 0xef;
+	auto Result{Get_SignedInteger_32Bit_BigEndian(*g_Buffer)};
 	
-	Inspection::Buffer Buffer{Data, 8};
+	assert(Result->GetSuccess() == true);
+	assert(std::experimental::any_cast< std::int32_t >(Result->GetAny()) == -559038737);
+}
+
+void Test_Get_SignedInteger_32Bit_LittleEndian(void)
+{
+	SetupDataAndBuffer();
 	
-	assert(Buffer.Get7Bits() == 0x6f);
-	assert(Buffer.GetPosition().GetBytes() == 0);
-	assert(Buffer.GetPosition().GetBits() == 7);
-	assert(Buffer.Get7Bits() == 0x2b);
-	assert(Buffer.GetPosition().GetBytes() == 1);
-	assert(Buffer.GetPosition().GetBits() == 6);
-	assert(Buffer.Get7Bits() == 0x37);
-	assert(Buffer.GetPosition().GetBytes() == 2);
-	assert(Buffer.GetPosition().GetBits() == 5);
-	assert(Buffer.Get7Bits() == 0x6e);
-	assert(Buffer.GetPosition().GetBytes() == 3);
-	assert(Buffer.GetPosition().GetBits() == 4);
-	assert(Buffer.Get7Bits() == 0x7e);
-	assert(Buffer.GetPosition().GetBytes() == 4);
-	assert(Buffer.GetPosition().GetBits() == 3);
-	assert(Buffer.Get7Bits() == 0x7a);
-	assert(Buffer.GetPosition().GetBytes() == 5);
-	assert(Buffer.GetPosition().GetBits() == 2);
-	assert(Buffer.Get7Bits() == 0x5b);
-	assert(Buffer.GetPosition().GetBytes() == 6);
-	assert(Buffer.GetPosition().GetBits() == 1);
-	assert(Buffer.Get7Bits() == 0x3e);
-	assert(Buffer.GetPosition().GetBytes() == 7);
-	assert(Buffer.GetPosition().GetBits() == 0);
+	auto Result{Get_SignedInteger_32Bit_LittleEndian(*g_Buffer)};
+	
+	assert(Result->GetSuccess() == true);
+	assert(std::experimental::any_cast< std::int32_t >(Result->GetAny()) == -272716322);
 }
 
 int main(int argc, char ** argv)
@@ -255,6 +257,8 @@ int main(int argc, char ** argv)
 	Test1Bit();
 	Test8BitsOffsetOne();
 	Test7Bits();
+	Test_Get_SignedInteger_32Bit_BigEndian();
+	Test_Get_SignedInteger_32Bit_LittleEndian();
 	
 	return 0;
 }
