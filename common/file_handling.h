e81ab74c739377a4d4cdf1cc2a8d04d13ff30fa0
Hagen Möbius <hagen.moebius@googlemail.com>
2017-04-08 17:08:27 +0200

    Moved the file handling function to the framework. Only a processor function object is passed to the framework, expected to retun a Result object that can be printed out. Sadly, id3inspector and mpeginspector are not yet up to date to use this correctly.

--- common/file_handling.h
+++ common/file_handling.h
@@ -2,9 +2,16 @@
 #define COMMON_FILE_HANDLING_H
 
 #include <dirent.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #include <iostream>
+
+void ReadDirectory(const std::string & Path, std::function< std::unique_ptr< Inspection::Result > (Inspection::Buffer &) > Processor);
+void ReadFile(const std::string & Path, std::function< std::unique_ptr< Inspection::Result > (Inspection::Buffer &) > Processor);
+void ReadItem(const std::string & Path, std::function< std::unique_ptr< Inspection::Result > (Inspection::Buffer &) > Processor);
 
 void ReadDirectory(const std::string & Path);
 void ReadFile(const std::string & Path);
@@ -49,7 +56,92 @@ inline bool IsRegularFile(const std::string & Path)
 	stat(Path.c_str(), &Stat);
 	
 	return S_ISREG(Stat.st_mode);
-} 
+}
+
+inline void ReadDirectory(const std::string & Path, std::function< std::unique_ptr< Inspection::Result > (Inspection::Buffer &) > Processor)
+{
+	DIR * Directory(opendir(Path.c_str()));
+	struct dirent * DirectoryEntry(0);
+	
+	while((DirectoryEntry = readdir(Directory)) != 0)
+	{
+		if((std::string(DirectoryEntry->d_name) != ".") && (std::string(DirectoryEntry->d_name) != ".."))
+		{
+			ReadItem(Path + '/' + DirectoryEntry->d_name, Processor);
+		}
+	}
+}
+
+inline void ReadItem(const std::string & Path, std::function< std::unique_ptr< Inspection::Result > (Inspection::Buffer &) > Processor)
+{
+	if(FileExists(Path) == true)
+	{
+		if(IsDirectory(Path) == true)
+		{
+			ReadDirectory(Path, Processor);
+		}
+		else if(IsRegularFile(Path) == true)
+		{
+			ReadFile(Path, Processor);
+		}
+		else
+		{
+			std::cerr << '"' << Path << "\" is no file or directory!" << std::endl;
+		}
+	}
+	else
+	{
+		std::cerr << '"' << Path << "\" does not exist!" << std::endl;
+		
+		return;
+	}
+}
+
+inline void ReadFile(const std::string & Path, std::function< std::unique_ptr< Inspection::Result > (Inspection::Buffer &) > Processor)
+{
+	auto FileDescriptor{open(Path.c_str(), O_RDONLY)};
+	
+	if(FileDescriptor == -1)
+	{
+		std::cerr << "Could not open the file \"" << Path << "\"." << std::endl;
+	}
+	else
+	{
+		std::int64_t FileSize{GetFileSize(Path)};
+		
+		if(FileSize != -1)
+		{
+			auto Address{reinterpret_cast< std::uint8_t * >(mmap(NULL, FileSize, PROT_READ, MAP_PRIVATE, FileDescriptor, 0))};
+			
+			if(Address == MAP_FAILED)
+			{
+				std::cerr << "Could not map the file \"" + Path + "\" into memory." << std::endl;
+			}
+			else
+			{
+				Inspection::Buffer Buffer{Address, Inspection::Length(FileSize, 0)};
+				auto ParseResult{Processor(Buffer)};
+				
+				PrintValue(ParseResult->GetValue());
+				if(ParseResult->GetSuccess() == false)
+				{
+					std::cerr << "The file does not start with a " << ParseResult->GetValue()->GetName() << '.' << std::endl;
+				}
+				else
+				{
+					auto Rest{Buffer.GetLength() - Buffer.GetPosition()};
+					
+					if(Rest > 0ull)
+					{
+						std::cerr << "There are " << Rest.GetBytes() << "." << Rest.GetBits() << " bytes and bits after the data." << std::endl;
+					}
+				}
+				munmap(Address, FileSize);
+			}
+		}
+		close(FileDescriptor);
+	}
+}
 
 inline void ReadDirectory(const std::string & Path)
 {
