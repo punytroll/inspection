bb3ead8a25c64ae8a710a0412146f6c06966ed1a
Hagen Möbius <hagen.moebius@googlemail.com>
2017-04-17 16:29:15 +0200

    Added an exception class to throw whenever features are not implemented yet.

--- common/5th.h
+++ common/5th.h
@@ -4,6 +4,7 @@
 #include "5th/buffer.h"
 #include "5th/getters.h"
 #include "5th/length.h"
+#include "5th/not_implemented_exception.h"
 #include "5th/result.h"
 #include "5th/value.h"
 #include "5th/value_printing.h"
