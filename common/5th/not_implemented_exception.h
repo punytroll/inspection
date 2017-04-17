e52201b897b818b630a85e5e3eb1e3e5e2ce13eb
Hagen M?bius <hagen.moebius@googlemail.com>
2017-04-17 16:29:15 +0200

    Added an exception class to throw whenever features are not implemented yet.

---
+++ common/5th/not_implemented_exception.h
@@ -0,0 +1,16 @@
+#ifndef INSPECTION_COMMON_NOT_IMPLEMENTED_EXCEPTION_H
+#define INSPECTION_COMMON_NOT_IMPLEMENTED_EXCEPTION_H
+
+namespace Inspection
+{
+	class NotImplementedException : public std::runtime_error
+	{
+	public:
+		NotImplementedException(const std::string & Message)
+			: std::runtime_error(Message)
+		{
+		}
+	};
+}
+
+#endif
