8aa1ae471b55a9f9cc98b54e5a168fd3c7e62d28
Hagen Möbius <hagen.moebius@googlemail.com>
2017-04-24 21:49:19 +0200

    Added one getter to get ISO/IEC 10464_1:1993 UCS-2 codepoints in big endian. Implemented three more getters: - one to get UCS-2 big endian characters. - one to get UCS-2 big endian strings ended by termination. - one to get UCS-2 big endian strings ended by termination or length. Fixed the UCS-2 little endian codepoint getter to be somewhat clearer on the value accumulation.

--- common/5th/getters.h
+++ common/5th/getters.h
@@ -38,6 +38,7 @@ namespace Inspection
 	std::unique_ptr< Inspection::Result > Get_ISO_IEC_10646_1_1993_UCS_2_ByteOrderMark(Inspection::Buffer & Buffer);
 	std::unique_ptr< Inspection::Result > Get_ISO_IEC_10646_1_1993_UCS_2_Character_BigEndian(Inspection::Buffer & Buffer);
 	std::unique_ptr< Inspection::Result > Get_ISO_IEC_10646_1_1993_UCS_2_Character_LittleEndian(Inspection::Buffer & Buffer);
+	std::unique_ptr< Inspection::Result > Get_ISO_IEC_10646_1_1993_UCS_2_CodePoint_BigEndian(Inspection::Buffer & Buffer);
 	std::unique_ptr< Inspection::Result > Get_ISO_IEC_10646_1_1993_UCS_2_CodePoint_LittleEndian(Inspection::Buffer & Buffer);
 	std::unique_ptr< Inspection::Result > Get_ISO_IEC_10646_1_1993_UCS_2_String_WithoutByteOrderMark_BigEndian_EndedByTermination(Inspection::Buffer & Buffer);
 	std::unique_ptr< Inspection::Result > Get_ISO_IEC_10646_1_1993_UCS_2_String_WithoutByteOrderMark_BigEndian_EndedByTerminationOrLength(Inspection::Buffer & Buffer, const Inspection::Length & Length);
