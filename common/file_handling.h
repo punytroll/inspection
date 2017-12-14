031d0230f68fd2481ebe1f0c7445e4ff7948ce93
Hagen M?bius <hagen.moebius@googlemail.com>
2017-12-14 18:37:19 +0100

    If no name is given at the top result value, the error message now falls back to a more general default.

--- common/file_handling.h
+++ common/file_handling.h
@@ -145,7 +145,14 @@ inline void ReadFile(const std::string & Path, std::function< std::unique_ptr< I
 				PrintValue(ParseResult->GetValue());
 				if(ParseResult->GetSuccess() == false)
 				{
-					std::cout << g_LightRed << "Parsing does not give valid " << ParseResult->GetValue()->GetName() << '.' << g_White << std::endl;
+					if(ParseResult->GetValue()->GetName() == "")
+					{
+						std::cout << g_LightRed << "Parsing does not give valid and complete result." << g_White << std::endl;
+					}
+					else
+					{
+						std::cout << g_LightRed << "Parsing does not give valid " << ParseResult->GetValue()->GetName() << '.' << g_White << std::endl;
+					}
 				}
 				
 				auto Rest{Buffer.GetLength() - Buffer.GetPosition()};
