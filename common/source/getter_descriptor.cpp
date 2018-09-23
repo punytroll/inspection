427a3257bc42cf019953ff13e611f91cea6ce410
Hagen M?bius <hagen.moebius@googlemail.com>
2018-09-24 00:47:09 +0200

    Re-added XML puny dom, because doing it within the sequential parser would require too many state variables. The DOM had to be extended by Text nodes and is more in line with the standard XML API. The GetterDescriptorParser was replaced by an internal implementation of a evaluating the DOM from the XML file. This now also works for getters that define parts (not just hardcoded getters). Removed getter implementation Get_ID3_2_2_Frame_Body_UFI(), which is now completely handled by the XML description of the getter and on-demand reading and parsing of the XML description. Removed SetHardcodedGetter() function as the parsing is now class-internal.

--- common/source/getter_descriptor.cpp
+++ common/source/getter_descriptor.cpp
@@ -1,95 +1,35 @@
 #include <fstream>
 
 #include "getter_descriptor.h"
+#include "getter_repository.h"
 #include "getters.h"
 #include "not_implemented_exception.h"
 #include "result.h"
-#include "xml_parser.h"
+#include "xml_puny_dom.h"
 
-class GetterDescriptionParser : public XMLParser
+namespace Inspection
 {
-public:
-	GetterDescriptionParser(Inspection::GetterDescriptor * GetterDescriptor, std::ifstream & InputFileStream) :
-		XMLParser{InputFileStream},
-		_GetterDescriptor{GetterDescriptor},
-		_InGetter{false},
-		_InHardcodedGetter{false}
+	enum class AppendType
 	{
-	}
-protected:
-	virtual void ElementStart(const std::string & TagName, const std::map< std::string, std::string > & Attributes)
-	{
-		if(TagName == "getter")
-		{
-			assert(_InGetter == false);
-			assert(_InHardcodedGetter == false);
-			_InGetter = true;
-		}
-		else if(TagName == "hardcoded-getter")
-		{
-			assert(_InGetter == true);
-			assert(_InHardcodedGetter == false);
-			_InHardcodedGetter = true;
-		}
-		else
-		{
-			assert(false);
-		}
-	}
-	
-	virtual void ElementEnd(const std::string & TagName)
-	{
-		if(TagName == "getter")
-		{
-			assert(_InGetter == true);
-			assert(_InHardcodedGetter == false);
-			_InGetter = false;
-		}
-		else if(TagName == "hardcoded-getter")
-		{
-			assert(_InGetter == true);
-			assert(_InHardcodedGetter == true);
-			_InHardcodedGetter = false;
-		}
-		else
-		{
-			assert(false);
-		}
-	}
-	
-	virtual void Text(const std::string & Text)
-	{
-		if(_InHardcodedGetter == true)
-		{
-			assert(_InGetter == true);
-			assert(_GetterDescriptor != nullptr);
-			if(Text == "Get_ASCII_String_Printable_EndedByTermination")
-			{
-				_GetterDescriptor->SetHardcodedGetter(Inspection::Get_ASCII_String_Printable_EndedByTermination);
-			}
-			else if(Text == "Get_Buffer_UnsignedInteger_8Bit_EndedByLength")
-			{
-				_GetterDescriptor->SetHardcodedGetter(Inspection::Get_Buffer_UnsignedInteger_8Bit_EndedByLength);
-			}
-		}
-	}
-private:
-	Inspection::GetterDescriptor * _GetterDescriptor;
-	bool _InGetter;
-	bool _InHardcodedGetter;
-};
+		Append
+	};
 
-void Inspection::GetterDescriptor::LoadGetterDescription(const std::string & GetterPath)
-{
-	std::ifstream InputFileStream{GetterPath};
-	GetterDescriptionParser Parser{this, InputFileStream};
-	
-	Parser.Parse();
+	class PartDescriptor
+	{
+	public:
+		std::vector< std::string > GetterModuleParts;
+		std::string GetterIdentifier;
+		Inspection::AppendType ValueAppendType;
+		std::string ValueName;
+	};
 }
 
-void Inspection::GetterDescriptor::SetHardcodedGetter(std::function< std::unique_ptr< Inspection::Result > (Inspection::Reader & Reader) > HardcodedGetter)
+Inspection::GetterDescriptor::~GetterDescriptor(void)
 {
-	_HardcodedGetter = HardcodedGetter;
+	for(auto PartDescriptor : _PartDescriptors)
+	{
+		delete PartDescriptor;
+	}
 }
 
 std::unique_ptr< Inspection::Result > Inspection::GetterDescriptor::Get(Inspection::Reader & Reader)
@@ -100,6 +40,174 @@ std::unique_ptr< Inspection::Result > Inspection::GetterDescriptor::Get(Inspecti
 	}
 	else
 	{
-		throw Inspection::NotImplementedException{"Only hard coded getters work."};
+		auto Result{Inspection::InitializeResult(Reader)};
+		auto Continue{true};
+		
+		// reading
+		for(auto PartDescriptor : _PartDescriptors)
+		{
+			Inspection::Reader PartReader{Reader};
+			auto PartResult{g_GetterRepository.Get(PartDescriptor->GetterModuleParts, PartDescriptor->GetterIdentifier, PartReader)};
+			
+			Continue = PartResult->GetSuccess();
+			if(PartDescriptor->ValueAppendType == Inspection::AppendType::Append)
+			{
+				Result->GetValue()->AppendValue(PartDescriptor->ValueName, PartResult->GetValue());
+			}
+			Reader.AdvancePosition(PartReader.GetConsumedLength());
+		}
+		// finalization
+		Result->SetSuccess(Continue);
+		Inspection::FinalizeResult(Result, Reader);
+		
+		return Result;
+	}
+}
+
+void Inspection::GetterDescriptor::LoadGetterDescription(const std::string & GetterPath)
+{
+	std::ifstream InputFileStream{GetterPath};
+	XML::Document Document{InputFileStream};
+	auto DocumentElement{Document.GetDocumentElement()};
+	
+	assert(DocumentElement != nullptr);
+	assert(DocumentElement->GetName() == "getter");
+	for(auto GetterChildNode : DocumentElement->GetChilds())
+	{
+		if(GetterChildNode->GetNodeType() == XML::NodeType::Element)
+		{
+			auto GetterChildElement{dynamic_cast< const XML::Element * >(GetterChildNode)};
+			
+			if(GetterChildElement->GetName() == "part")
+			{
+				auto PartDescriptor{new Inspection::PartDescriptor{}};
+				
+				for(auto PartChildNode : GetterChildElement->GetChilds())
+				{
+					if(PartChildNode->GetNodeType() == XML::NodeType::Element)
+					{
+						auto PartChildElement{dynamic_cast< const XML::Element * >(PartChildNode)};
+						
+						if(PartChildElement->GetName() == "getter")
+						{
+							for(auto PartGetterChildNode : PartChildElement->GetChilds())
+							{
+								if(PartGetterChildNode->GetNodeType() == XML::NodeType::Element)
+								{
+									auto PartGetterChildElement{dynamic_cast< const XML::Element * >(PartGetterChildNode)};
+									
+									if(PartGetterChildElement->GetName() == "module")
+									{
+										for(auto PartGetterModuleChildNode : PartGetterChildElement->GetChilds())
+										{
+											if(PartGetterModuleChildNode->GetNodeType() == XML::NodeType::Element)
+											{
+												auto PartGetterModuleChildElement{dynamic_cast< const XML::Element * >(PartGetterModuleChildNode)};
+												
+												if(PartGetterModuleChildElement->GetName() == "part")
+												{
+													assert(PartGetterModuleChildElement->GetChilds().size() == 1);
+													
+													auto ModulePartText{dynamic_cast< const XML::Text * >(PartGetterModuleChildElement->GetChild(0))};
+													
+													assert(ModulePartText != nullptr);
+													PartDescriptor->GetterModuleParts.push_back(ModulePartText->GetText());
+												}
+												else
+												{
+													throw std::domain_error{PartGetterModuleChildElement->GetName()};
+												}
+											}
+										}
+									}
+									else if(PartGetterChildElement->GetName() == "identifier")
+									{
+										assert(PartGetterChildElement->GetChilds().size() == 1);
+										
+										auto IdentifierText{dynamic_cast< const XML::Text * >(PartGetterChildElement->GetChild(0))};
+										
+										assert(IdentifierText != nullptr);
+										PartDescriptor->GetterIdentifier = IdentifierText->GetText();
+									}
+									else
+									{
+										throw std::domain_error{PartGetterChildElement->GetName()};
+									}
+								}
+							}
+						}
+						else if(PartChildElement->GetName() == "value")
+						{
+							for(auto PartValueChildNode : PartChildElement->GetChilds())
+							{
+								if(PartValueChildNode->GetNodeType() == XML::NodeType::Element)
+								{
+									auto PartValueChildElement{dynamic_cast< const XML::Element * >(PartValueChildNode)};
+									
+									if(PartValueChildElement->GetName() == "append")
+									{
+										PartDescriptor->ValueAppendType = Inspection::AppendType::Append;
+										for(auto PartValueAppendChildNode : PartValueChildElement->GetChilds())
+										{
+											if(PartValueAppendChildNode->GetNodeType() == XML::NodeType::Element)
+											{
+												auto PartValueAppendChildElement{dynamic_cast< const XML::Element * >(PartValueAppendChildNode)};
+												
+												if(PartValueAppendChildElement->GetName() == "name")
+												{
+													assert(PartValueAppendChildElement->GetChilds().size() == 1);
+													
+													auto NameText{dynamic_cast< const XML::Text * >(PartValueAppendChildElement->GetChild(0))};
+													
+													assert(NameText != nullptr);
+													PartDescriptor->ValueName = NameText->GetText();
+												}
+												else
+												{
+													throw std::domain_error{PartValueAppendChildElement->GetName()};
+												}
+											}
+										}
+									}
+									else
+									{
+										throw std::domain_error{PartValueChildElement->GetName()};
+									}
+								}
+							}
+						}
+						else
+						{
+							throw std::domain_error{PartChildElement->GetName()};
+						}
+					}
+				}
+				_PartDescriptors.push_back(PartDescriptor);
+			}
+			else if(GetterChildElement->GetName() == "hardcoded-getter")
+			{
+				assert(GetterChildElement->GetChilds().size() == 1);
+				
+				auto HardcodedGetterText{dynamic_cast< const XML::Text * >(GetterChildElement->GetChild(0))};
+				
+				assert(HardcodedGetterText != nullptr);
+				if(HardcodedGetterText->GetText() == "Get_ASCII_String_Printable_EndedByTermination")
+				{
+					_HardcodedGetter = Inspection::Get_ASCII_String_Printable_EndedByTermination;
+				}
+				else if(HardcodedGetterText->GetText() == "Get_Buffer_UnsignedInteger_8Bit_EndedByLength")
+				{
+					_HardcodedGetter = Inspection::Get_Buffer_UnsignedInteger_8Bit_EndedByLength;
+				}
+				else
+				{
+					throw std::domain_error{HardcodedGetterText->GetText()};
+				}
+			}
+			else
+			{
+				throw std::domain_error{GetterChildElement->GetName()};
+			}
+		}
 	}
 }
--- common/source/getter_descriptor.h
+++ common/source/getter_descriptor.h
@@ -3,20 +3,23 @@
 
 #include <functional>
 #include <memory>
+#include <vector>
 
 namespace Inspection
 {
+	class PartDescriptor;
 	class Reader;
 	class Result;
 	
 	class GetterDescriptor
 	{
 	public:
-		void SetHardcodedGetter(std::function< std::unique_ptr< Inspection::Result > (Inspection::Reader & Reader) > HardcodedGetter);
-		void LoadGetterDescription(const std::string & GetterPath);
+		~GetterDescriptor(void);
 		std::unique_ptr< Inspection::Result > Get(Inspection::Reader & Reader);
+		void LoadGetterDescription(const std::string & GetterPath);
 	private:
 		std::function< std::unique_ptr< Inspection::Result > (Inspection::Reader & Reader) > _HardcodedGetter;
+		std::vector< Inspection::PartDescriptor * > _PartDescriptors;
 	};
 }
 
