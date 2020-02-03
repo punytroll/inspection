95a3ef41945500f169758fc7680e6bb2fa7a7db7
Hagen Möbius <hagen.moebius@googlemail.com>
2020-02-03 09:32:28 +0100

    Added some error handling for an ID3v1 tag.

--- id3/id3inspector.cpp
+++ id3/id3inspector.cpp
@@ -9,6 +9,8 @@
 
 using namespace std::string_literals;
 
+auto g_ID3v1{false};
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // application                                                                                   //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -16,38 +18,61 @@ using namespace std::string_literals;
 std::unique_ptr< Inspection::Result > Process(Inspection::Reader & Reader)
 {
 	auto Result{Inspection::InitializeResult(Reader)};
+	auto Continue{true};
 	
 	Result->GetValue()->SetName("ID3Tags");
 	
-	std::unique_ptr< Inspection::Result > ID3v1TagResult;
-	
-	if(Reader.GetCompleteLength() >= Inspection::Length(128, 0))
+	if(g_ID3v1 == true)
 	{
 		Reader.SetPosition(Reader.GetCompleteLength() - Inspection::Length(128, 0));
 		
 		Inspection::Reader PartReader{Reader};
+		auto PartResult{Inspection::g_TypeRepository.Get({"ID3", "v1", "Tag"}, PartReader, {})};
 		
-		ID3v1TagResult = Inspection::g_TypeRepository.Get({"ID3", "v1", "Tag"}, PartReader, {});
-		if(ID3v1TagResult->GetSuccess() == true)
+		Continue = PartResult->GetSuccess();
+		if(PartResult->GetValue()->HasField("AlbumTrack") == true)
 		{
-			if(ID3v1TagResult->GetValue()->HasField("AlbumTrack") == true)
-			{
-				Result->GetValue()->AppendField("ID3v1.1", ID3v1TagResult->GetValue());
-			}
-			else
-			{
-				Result->GetValue()->AppendField("ID3v1", ID3v1TagResult->GetValue());
-			}
+			Result->GetValue()->AppendField("ID3v1.1", PartResult->GetValue());
+		}
+		else
+		{
+			Result->GetValue()->AppendField("ID3v1", PartResult->GetValue());
 		}
 	}
-	Reader.SetPosition(Inspection::Length(0, 0));
-	
-	Inspection::Reader PartReader{Reader};
-	auto ID3v2TagResult{Inspection::Get_ID3_2_Tag(PartReader, {})};
-	
-	Result->GetValue()->AppendField("ID3v2", ID3v2TagResult->GetValue());
-	Result->SetSuccess(((ID3v1TagResult != nullptr) && (ID3v1TagResult->GetSuccess() == true)) || ID3v2TagResult->GetSuccess());
+	else
+	{
+		std::unique_ptr< Inspection::Result > ID3v1TagResult;
+		
+		if(Reader.GetCompleteLength() >= Inspection::Length(128, 0))
+		{
+			Reader.SetPosition(Reader.GetCompleteLength() - Inspection::Length(128, 0));
+			
+			Inspection::Reader PartReader{Reader};
+			
+			ID3v1TagResult = Inspection::g_TypeRepository.Get({"ID3", "v1", "Tag"}, PartReader, {});
+			if(ID3v1TagResult->GetSuccess() == true)
+			{
+				if(ID3v1TagResult->GetValue()->HasField("AlbumTrack") == true)
+				{
+					Result->GetValue()->AppendField("ID3v1.1", ID3v1TagResult->GetValue());
+				}
+				else
+				{
+					Result->GetValue()->AppendField("ID3v1", ID3v1TagResult->GetValue());
+				}
+			}
+		}
+		Reader.SetPosition(Inspection::Length(0, 0));
+		
+		Inspection::Reader PartReader{Reader};
+		auto ID3v2TagResult{Inspection::Get_ID3_2_Tag(PartReader, {})};
+		
+		Result->GetValue()->AppendField("ID3v2", ID3v2TagResult->GetValue());
+		Continue = ((ID3v1TagResult != nullptr) && (ID3v1TagResult->GetSuccess() == true)) || ID3v2TagResult->GetSuccess();
+	}
 	Reader.SetPosition(Reader.GetCompleteLength());
+	// finalization
+	Result->SetSuccess(Continue);
 	Inspection::FinalizeResult(Result, Reader);
 	
 	return Result;
@@ -56,12 +81,21 @@ std::unique_ptr< Inspection::Result > Process(Inspection::Reader & Reader)
 int main(int argc, char **argv)
 {
 	std::deque< std::string > Paths;
-	unsigned int Arguments(argc);
-	unsigned int Argument(0);
+	auto Arguments{argc};
+	auto ArgumentIndex{0};
 
-	while(++Argument < Arguments)
+	while(++ArgumentIndex < Arguments)
 	{
-		Paths.push_back(argv[Argument]);
+		std::string Argument{argv[ArgumentIndex]};
+		
+		if(Argument == "--id3v1")
+		{
+			g_ID3v1 = true;
+		}
+		else
+		{
+			Paths.push_back(Argument);
+		}
 	}
 	if(Paths.size() == 0)
 	{
