453aa9c7a3c996e3c344e9443874d300f90674c0
Hagen Möbius <hagen.moebius@googlemail.com>
2021-07-02 00:49:21 +0200

    All python scripts are now indented using spaces instead of tabs.

--- test/test.py
+++ test/test.py
@@ -27,35 +27,35 @@ parser.add_option("-i", "--in", dest="test_suite", help="The test suite file.")
 # read the options and validate
 (options, args) = parser.parse_args()
 if options.test_suite == None:
-	print("Set a test suite with '--in'.")
-	exit(1)
+    print("Set a test suite with '--in'.")
+    exit(1)
 
 def get_text(nodes):
-	texts = list()
-	for node in nodes:
-		if node.nodeType == node.TEXT_NODE:
-			texts.append(node.data)
-	return "".join(texts)
+    texts = list()
+    for node in nodes:
+        if node.nodeType == node.TEXT_NODE:
+            texts.append(node.data)
+    return "".join(texts)
 
 def get_space_appended(list):
-	return " ".join(list)
+    return " ".join(list)
 
 def get_s_if_greater_one(number):
-	if number > 1:
-		return "s"
-	else:
-		return ""
+    if number > 1:
+        return "s"
+    else:
+        return ""
 
 class Execute(object):
-	def __init__(self):
-		self.command = None
-		self.arguments = list()
+    def __init__(self):
+        self.command = None
+        self.arguments = list()
 
 class Test(object):
-	def __init__(self):
-		self.description = None
-		self.expected_output = None
-		self.execute = Execute()
+    def __init__(self):
+        self.description = None
+        self.expected_output = None
+        self.execute = Execute()
 
 tests = list()
 
@@ -63,49 +63,49 @@ tests = list()
 test_suite_document = parse(options.test_suite)
 tests_element = test_suite_document.documentElement
 for node in tests_element.childNodes:
-	if node.nodeType == Node.ELEMENT_NODE and node.tagName == "test":
-		test_element = node
-		test = Test()
-		for node in test_element.childNodes:
-			if node.nodeType == Node.ELEMENT_NODE:
-				if node.tagName == "description":
-					test.description = get_text(node.childNodes)
-				elif node.tagName == "execute":
-					execute_element = node
-					for node in execute_element.childNodes:
-						if node.nodeType == Node.ELEMENT_NODE:
-							if node.tagName == "command":
-								test.execute.command = get_text(node.childNodes)
-							elif node.tagName == "argument":
-								test.execute.arguments.append(get_text(node.childNodes))
-				elif node.tagName == "expected-output":
-					test.expected_output = get_text(node.childNodes)
-		tests.append(test)
+    if node.nodeType == Node.ELEMENT_NODE and node.tagName == "test":
+        test_element = node
+        test = Test()
+        for node in test_element.childNodes:
+            if node.nodeType == Node.ELEMENT_NODE:
+                if node.tagName == "description":
+                    test.description = get_text(node.childNodes)
+                elif node.tagName == "execute":
+                    execute_element = node
+                    for node in execute_element.childNodes:
+                        if node.nodeType == Node.ELEMENT_NODE:
+                            if node.tagName == "command":
+                                test.execute.command = get_text(node.childNodes)
+                            elif node.tagName == "argument":
+                                test.execute.arguments.append(get_text(node.childNodes))
+                elif node.tagName == "expected-output":
+                    test.expected_output = get_text(node.childNodes)
+        tests.append(test)
 
 magnitude_of_number_of_tests = len(str(len(tests)))
 number_of_tests = 0
 number_of_successes = 0
 number_of_failures = 0
 for test in tests:
-	number_of_tests += 1
-	if test.description != None:
-		print(f"{BrightWhite}[{BrightBlue}{str(number_of_tests).zfill(magnitude_of_number_of_tests)}{BrightWhite}]{Reset} {BrightYellow}{test.description}{Reset}")
-	else:
-		print(f"{BrightWhite}[{BrightBlue}{str(number_of_tests).zfill(magnitude_of_number_of_tests)}{BrightWhite}]{Reset}")
-	print(f"    Running \"{test.execute.command} {get_space_appended(test.execute.arguments)}\"")
-	result = subprocess.run([test.execute.command] + test.execute.arguments, stdout=subprocess.PIPE)
-	result = result.stdout.decode("utf-8")
-	if result == test.expected_output:
-		number_of_successes += 1
-		print(f"        => \"{BrightGreen}{result}{Reset}\"")
-	else:
-		number_of_failures += 1
-		print(f"        => \"{BrightRed}{result}{Reset}\"")
-		print(f"    Test failed! Expected output was \"{BrightBlue}{test.expected_output}{Reset}\".")
-	print()
+    number_of_tests += 1
+    if test.description != None:
+        print(f"{BrightWhite}[{BrightBlue}{str(number_of_tests).zfill(magnitude_of_number_of_tests)}{BrightWhite}]{Reset} {BrightYellow}{test.description}{Reset}")
+    else:
+        print(f"{BrightWhite}[{BrightBlue}{str(number_of_tests).zfill(magnitude_of_number_of_tests)}{BrightWhite}]{Reset}")
+    print(f"    Running \"{test.execute.command} {get_space_appended(test.execute.arguments)}\"")
+    result = subprocess.run([test.execute.command] + test.execute.arguments, stdout=subprocess.PIPE)
+    result = result.stdout.decode("utf-8")
+    if result == test.expected_output:
+        number_of_successes += 1
+        print(f"        => \"{BrightGreen}{result}{Reset}\"")
+    else:
+        number_of_failures += 1
+        print(f"        => \"{BrightRed}{result}{Reset}\"")
+        print(f"    Test failed! Expected output was \"{BrightBlue}{test.expected_output}{Reset}\".")
+    print()
 print()
 if number_of_failures == 0:
-	print(f"All {Yellow}{str(number_of_successes)}{Reset} test{get_s_if_greater_one(number_of_successes)} {BrightGreen}succeeded{Reset}.")
+    print(f"All {Yellow}{str(number_of_successes)}{Reset} test{get_s_if_greater_one(number_of_successes)} {BrightGreen}succeeded{Reset}.")
 else:
-	print(f"Out of {Yellow}{str(number_of_tests)}{Reset} test{get_s_if_greater_one(number_of_tests)}, {Yellow}{str(number_of_successes)}{Reset} test{get_s_if_greater_one(number_of_successes)} {BrightGreen}succeeded{Reset} and {Yellow}{str(number_of_failures)}{Reset} test{get_s_if_greater_one(number_of_failures)} {BrightRed}failed{Reset}.")
-	exit(1)
+    print(f"Out of {Yellow}{str(number_of_tests)}{Reset} test{get_s_if_greater_one(number_of_tests)}, {Yellow}{str(number_of_successes)}{Reset} test{get_s_if_greater_one(number_of_successes)} {BrightGreen}succeeded{Reset} and {Yellow}{str(number_of_failures)}{Reset} test{get_s_if_greater_one(number_of_failures)} {BrightRed}failed{Reset}.")
+    exit(1)
