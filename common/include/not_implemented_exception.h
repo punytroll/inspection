864a3beaadc4468eaaf7fd1af4e32b306593731b
Hagen Möbius <hagen.moebius@googlemail.com>
2021-07-25 22:19:17 +0200

    Fixed missing includes in not_implemented_exception.h.

--- common/include/not_implemented_exception.h
+++ common/include/not_implemented_exception.h
@@ -1,6 +1,9 @@
 #ifndef INSPECTION_COMMON_NOT_IMPLEMENTED_EXCEPTION_H
 #define INSPECTION_COMMON_NOT_IMPLEMENTED_EXCEPTION_H
 
+#include <stdexcept>
+#include <string>
+
 namespace Inspection
 {
 	class NotImplementedException : public std::runtime_error
